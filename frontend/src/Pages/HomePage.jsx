3dfa3cad0ee15c4dfa64abe1fcaf030f243034f5
govindop07 <govindupa24@gmail.com>
2025-07-07 12:13:37 +0530

    [Govind] completed the homepage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import LatestCollection from '../components/LatestCollection'
+import BestSellers from '../components/BestSellers'
+import OurPolicy from '../components/OurPolicy'
+import Footer from '../components/Footer'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <LatestCollection />
+      <BestSellers />
+      <OurPolicy />
+    </div>
   )
 }
 
