ddf9107138533bffb6aa6ea5aa71e1db6f2b777a
govindop07 <govindupa24@gmail.com>
2025-07-31 21:44:16 +0530

    [Govind] Connected cart's all operations with backend

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { addToCart, getUserCart, updateCart } from '../controllers/cart.controller.js';
+import authUser from '../middleware/auth.js';
+const cartRouter = express.Router();
+
+cartRouter.post('/add', authUser, addToCart);
+cartRouter.post('/get', authUser, getUserCart);
+cartRouter.post('/update', authUser, updateCart);
+cartRouter.get('/test', (req, res) => {
+  res.send("api working");
+});
+
+export default cartRouter;
